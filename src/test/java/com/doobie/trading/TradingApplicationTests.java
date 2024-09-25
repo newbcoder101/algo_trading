a6f5c9e2a9c7c560cba3f8f5167ed42ab4a62fbe
newbcoder101 <omkarsm101@gmail.com>
2024-09-25 09:13:31 +0530

    initial commit-msg

---
+++ src/test/java/com/doobie/trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doobie.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
